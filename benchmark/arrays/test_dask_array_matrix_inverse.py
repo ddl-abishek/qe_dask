ff9a63b4191ffe6741ce50294236dced2dc69712
Abishek Subramanian <abishek.subramanian@dominodatalab.com>
2021-07-06 20:49:02 +0000

    increased chunk sizee

--- benchmark/arrays/test_dask_array_matrix_inverse.py
+++ benchmark/arrays/test_dask_array_matrix_inverse.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     
     start = datetime.now()    
     #creating a 2000x2000 array whose values are normally distributed with zero mean and unit variance
-    a = da.random.normal(0,1,size=(200, 200),chunks=(10,10))
+    a = da.random.normal(0,1,size=(200, 200),chunks=(100,100))
     #computing the inverse of the array
     inv_a = da.linalg.inv(a).compute()
     print(inv_a)

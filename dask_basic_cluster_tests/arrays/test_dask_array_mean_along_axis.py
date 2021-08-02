f685a1c5f5c51ba32a0a4dce1c8a62c9350c1826
ddl-abishek <71837717+ddl-abishek@users.noreply.github.com>
2021-08-02 23:19:12 +0530

    Update test_dask_array_mean_along_axis.py

--- dask_basic_cluster_tests/arrays/test_dask_array_mean_along_axis.py
+++ dask_basic_cluster_tests/arrays/test_dask_array_mean_along_axis.py
@@ -22,6 +22,4 @@ if __name__ == '__main__':
     with performance_report(filename=f"/mnt/artifacts/results/dask-report_test_dask_array_mean_along_axis_{str(datetime.now())}.html"):
         dask_submit = client.submit(test_array, 1)
         print(dask_submit.result())
-
-
-
+    os.system(f"cp /mnt/artifacts/results/dask-report_test_dask_array_mean_along_axis_{str(datetime.now())}.html /mnt/code") 

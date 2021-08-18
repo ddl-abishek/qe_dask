20311be324fba99e61f3756b90c48ee5783862d5
ddl-abishek <71837717+ddl-abishek@users.noreply.github.com>
2021-08-18 16:09:29 +0530

    Update test_dask_bag.py

--- dask_basic_cluster_tests/bags/test_dask_bag.py
+++ dask_basic_cluster_tests/bags/test_dask_bag.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     filename = f"/mnt/artifacts/results/dask-report_test_dask_bag_{str(datetime.now())}.html"    
     with performance_report(filename=filename):
         dask_map = client.map(test_bag, range(10))
-            print(client.gather(dask_map))
+        print(client.gather(dask_map))
 
     os.system(f"cp {filename} /mnt/code")
     client.restart()

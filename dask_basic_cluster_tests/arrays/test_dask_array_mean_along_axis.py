74045b32dfb49aef659ef6a2447d9b1cf633b5bd
ddl-abishek <71837717+ddl-abishek@users.noreply.github.com>
2021-08-18 16:01:24 +0530

    Update test_dask_array_mean_along_axis.py

--- dask_basic_cluster_tests/arrays/test_dask_array_mean_along_axis.py
+++ dask_basic_cluster_tests/arrays/test_dask_array_mean_along_axis.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     service_host = os.environ['DASK_SCHEDULER_SERVICE_HOST']
     
     client = Client(address=f'{service_host}:{service_port}')
-    client.wait_for_workers(n_workers=4)
+    client.wait_for_workers(n_workers=3)
     client.restart()
     
     filename=f"/mnt/artifacts/results/dask-report_test_dask_array_mean_along_axis_{str(datetime.now())}.html".replace(' ','')
